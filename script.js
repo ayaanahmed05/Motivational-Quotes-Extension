9ec093e3b6e138d6101695bf727c1ca84c98a9ee
ayaanahmed05 <ayaan.ahmed@ontariotechu.net>
2025-01-09 19:31:57 -0500

    Added ZenQuotes.io API
    Added api calls in order to display a random quote, uses ZenQuotes API and cors-anywhere to use Access-Control-Allow-Origin headers

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// element selectors
-const quoteParagraph = document.getElementById("quote-text");
-const refreshButton = document.getElementById("refresh-button");
-
 // random quotes
 // TODO: store quotes and authors in a 2d array ex [["quote"], ["author"]]
 const quotes = [
@@ -12,27 +8,71 @@ const quotes = [
     "\"The road to success and the road to failure are almost exactly the same.\" - Colin R. Davis"
 ]
 
+// element selectors
+const quoteParagraph = document.getElementById("quote-text");
+const refreshButton = document.getElementById("refresh-button");
+
 // returns a random integer between main (inclusive) and max (included)
 function getRandomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-function refresh() {
-    if (quoteParagraph === null) {
-        alert("null element");
-    } else {
-        quoteParagraph.innerText = quotes[getRandomInteger(0, quotes.length - 1)] || "Undefined";
+// API Fetching
+
+/* 
+    We are using ZenQuotes.io to provide our quotes for us, and we 
+    cannot use Access-Control-Allow-Origin headers without a premium key. 
+    As a result, we are using https://cors-anywhere.herokuapp.com/ to 
+    allow us to use this API. 
+
+    This project is purely for learning purposes, and I have no intentions 
+    of monetizing this in any way. 
+
+    In order for this to work, you must click on the link above and request 
+    temporary access for the API calls to work. Visit:
+    https://cors-anywhere.herokuapp.com/corsdemo
+    and click the "Request temporary access" button.
+*/
+async function fetchQuote() {
+    const proxyURL = "https://cors-anywhere.herokuapp.com/"
+    const apiURL = "https://zenquotes.io/api/random"
+
+    try {
+        const response = await fetch(proxyURL + apiURL);
+        if (!response.ok) {
+            throw new Error("Response is not ok");
+        }
+
+        const data = await response.json();
+        const quote = data[0].q;
+        const author = data[0].a;
+
+        console.log(`${quote} - ${author}`);
+        console.log(data);
+
+        return data[0];
+    }
+    catch (error) {
+        console.error("There is a problem fetching the quote");
     }
 }
 
-if (quoteParagraph === null) {
-    alert("null element");
-} else {
-    quoteParagraph.innerText = quotes[getRandomInteger(0, quotes.length - 1)] || "Undefined";
+async function displayQuote() {
+    // get quote from api and wait for it to return
+    const quoteData = await fetchQuote();
+
+    if (quoteParagraph === null) {
+        alert("null element");
+    } else {
+        quoteParagraph.innerHTML = `<em>${quoteData.q}</em> &mdash; ${quoteData.a}` || "Undefined";
+    }
 }
+
 
 if (refreshButton === null) {
     alert("null element");
 } else {
-    refreshButton.onclick = refresh;
+    refreshButton.addEventListener("click", displayQuote);
 }
+
+displayQuote();
